899a88f7af24785425944b31bab9d2a1407318ca
kevin <kevin@DESKTOP-B53C1K7>
2020-11-21 15:53:11 -0800

    Add getBalance() and deduct() tests.

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,32 @@
+package csc131.junit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+	
+public class GiftCardTest
+{
+	@Test
+	public void getIssuingStore()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("getIssuingStore()",
+		issuingStore, card.getIssuingStore());
+	}
+	@Test
+	public void getBalance()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore(), 0.001);
+	}
+}
